393aa56fbb44e35ed6e4e06a887bcd02141a677f
Wesley <0000110206841xsp@al.educacao.sp.gov.br>
2024-11-27 23:17:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarega'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Oi em inglês?',
+    'Oi em inglês é HI'
+)
